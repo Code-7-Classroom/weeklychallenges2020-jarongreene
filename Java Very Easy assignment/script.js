a68a3ccc6fbebb0de3eb8a74a660351d2834cdd8
Jaron Greene <jarongreene5@gmail.com>
2020-03-24 09:43:36 -0400

    advance css and javascript challenges

---
+++ Java Very Easy assignment/script.js
@@ -0,0 +1,35 @@
+//Very Easy challenge
+
+var num10 = 10;
+
+var num20 = 20;
+
+var total = num20 - num10;
+
+console.log(total)
+
+console.log('The difference between' , + num20 +  ' and ' + num10 + ' is '  + total)
+
+
+//Easy challenge 
+
+var firstName = "Jaron";
+
+var lastName = "Greene"
+
+console.log('The name ' + lastName + ' is longer than ' + firstName + ' by 1 letter')
+
+//Medium Challenge
+
+var input = prompt('Enter a word');
+
+if (input === input.toUpperCase()) {
+    console.log("You are shouting!")
+} else if (input === input.toLowerCase()){
+    console.log("You are whispering.")
+} else { 
+     console.log("You are speaking normally.")
+ }
+
+
+ 
